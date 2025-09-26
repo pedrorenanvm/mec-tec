fdb48afa50414e371bb95eb17a000572d8d21da0
Pedro Renan <pedroviana15094@gmail.com>
2025-09-26 17:09:47 -0300

    feat: initial commit

---
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/MecTecApplication.java
@@ -0,0 +1,13 @@
+package com.br.edu.ufersa.prog_web.mec_tec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/edu/ufersa/prog_web/mec_tec/MecTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.edu.ufersa.prog_web.mec_tec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
