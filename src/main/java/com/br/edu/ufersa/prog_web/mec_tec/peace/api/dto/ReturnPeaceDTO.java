ab01de452baa8b537efa810605546dcc0e2da2c0
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-10-03 16:01:06 -0300

    feat: add DTOS to use in application

---
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/peace/api/dto/ReturnPeaceDTO.java
@@ -0,0 +1,20 @@
+package com.br.edu.ufersa.prog_web.mec_tec.peace.api.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReturnPeaceDTO {
+    private UUID id;
+    private String name;
+    private int amount;
+    private String brand;
+    private String description;
+}
