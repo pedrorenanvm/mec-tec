069706110b08b03c8fa8cad87390326d17e54b63
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-10-03 16:01:20 -0300

    feat: add Entity to use in application

---
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/peace/model/entity/Peace.java
@@ -0,0 +1,34 @@
+package com.br.edu.ufersa.prog_web.mec_tec.peace.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "tb_peace")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Peace {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int amount;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String description;
+}
