80189b20220345fe59799817140c6b44f95209cd
Pedro Renan <pedroviana15094@gmail.com>
2025-10-08 14:05:57 -0300

    fix: machine entity corrected

--- src/main/java/com/br/edu/ufersa/prog_web/mec_tec/machine/model/entity/Machine.java
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/machine/model/entity/Machine.java
@@ -1,13 +1,17 @@
 package com.br.edu.ufersa.prog_web.mec_tec.machine.model.entity;
 
 import com.br.edu.ufersa.prog_web.mec_tec.customer.model.entity.Customer;
+import com.br.edu.ufersa.prog_web.mec_tec.task.model.entity.Task;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -38,5 +42,9 @@ public class Machine {
     @JsonBackReference
     @JoinColumn(name = "customer_id", nullable = false) // fk na tabela
     private Customer customer;
+
+    @OneToMany(mappedBy = "machine", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference // Para evitar loops no JSON
+    private List<Task> tasks = new ArrayList<>();
 
 }
