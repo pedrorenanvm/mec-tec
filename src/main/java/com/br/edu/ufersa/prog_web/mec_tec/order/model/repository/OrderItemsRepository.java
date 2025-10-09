d646195db44e1cd08c39be93da8f3c1d5391cc87
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-10-09 16:41:22 -0300

    feat: add Repositories of entities of module Order

---
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/order/model/repository/OrderItemsRepository.java
@@ -0,0 +1,12 @@
+package com.br.edu.ufersa.prog_web.mec_tec.order.model.repository;
+
+import com.br.edu.ufersa.prog_web.mec_tec.order.model.entity.Order_Items;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderItemsRepository extends JpaRepository<Order_Items, UUID> {
+    List<Order_Items> findByOrderId(Long orderId);
+    List<Order_Items> findByTaskId(UUID taskId);
+}
