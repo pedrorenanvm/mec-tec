b41ee7a4f8625d2d00a4d0e9adc9fa7ce04efa88
Pedro Renan <pedroviana15094@gmail.com>
2025-10-15 14:58:22 -0300

    feat: add findByModel, finByBrand, findByCustomerName

--- src/main/java/com/br/edu/ufersa/prog_web/mec_tec/machine/model/repository/MachineRepository.java
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/machine/model/repository/MachineRepository.java
@@ -1,7 +1,11 @@
 package com.br.edu.ufersa.prog_web.mec_tec.machine.model.repository;
 
 import com.br.edu.ufersa.prog_web.mec_tec.machine.model.entity.Machine;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,8 +13,16 @@ import java.util.UUID;
 @Repository
 public interface MachineRepository extends JpaRepository<Machine, UUID> {
 
-//    Optional<Machine> findByName(String name);
-//    Optional<Machine> findByBrand(String brand);
-//    Optional<Machine> findByCustomer(Customer customer);
+    // busca por modelo
+    @Query("SELECT m FROM Machine m WHERE LOWER(m.model) LIKE LOWER(CONCANT('%', :model, '%'))")
+    Page<Machine> findByModel(@Param("model") String model, Pageable pageable);
+
+    //busca por brand
+    @Query("SELECT m FROM Machine m WHERE LOWER(m.brand) LIKE LOWER(CONCANT('%', :brand, '%'))")
+    Page<Machine> findByBrand(@Param("brand") String brand , Pageable pageable);
+
+    // busca por cliente
+    @Query("SELECT m FROM Machine m JOIN m.customer c WHERE LOWER(c.name) LIKE LOWER(CONCANT('%', :customerName, '%'))")
+    Page<Machine> findByCustomerName(@Param("customerName") String customerName, Pageable pageable);
 
 }
