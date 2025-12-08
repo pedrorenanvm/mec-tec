9112a1da5121a9aa3922f4ae82f81175827df045
elnatanorato <elnatan.honorato@gmail.com>
2025-12-08 13:43:20 -0300

    fix: fix password

--- src/main/java/com/br/edu/ufersa/prog_web/mec_tec/config/SecurityConfig.java
+++ src/main/java/com/br/edu/ufersa/prog_web/mec_tec/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -42,13 +40,7 @@ public class SecurityConfig {
                 // Adiciona o tratamento de erro customizado para falhas de autenticação
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(customAuthenticationEntryPoint));
 
-
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
     }
 
     @Bean
